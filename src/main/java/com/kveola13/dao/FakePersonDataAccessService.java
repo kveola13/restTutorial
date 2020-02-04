3d333f9a387d2c825bc44d97afcb2c557da31b6a
Ola Kveli <olakveli@yahoo.com>
2020-02-04 21:32:35 +0100

    init, added some boilerplate and a post request

---
+++ src/main/java/com/kveola13/dao/FakePersonDataAccessService.java
@@ -0,0 +1,21 @@
+package com.kveola13.dao;
+
+import com.kveola13.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+
+    private static List<Person> Database = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        Database.add(new Person(id, person.getName()));
+        return 1;
+    }
+
+}
---
+++ src/main/java/com/kveola13/dao/PersonDao.java
@@ -0,0 +1,16 @@
+package com.kveola13.dao;
+
+import com.kveola13.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+}
